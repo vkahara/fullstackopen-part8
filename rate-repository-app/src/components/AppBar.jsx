9ba3152295901b70b6738af62e6a803966b7e7cf
vkahara <valtterikahara@gmail.com>
2025-12-01 11:24:46 +0200

    exercise 10.7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,35 +1,39 @@
-import { View, StyleSheet, Pressable, Alert } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 import theme from "../theme";
 import Subheading from "./Subheading";
 import { Link } from "react-router-native";
+import { ScrollView } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
     paddingTop: Constants.statusBarHeight,
-    paddingLeft: 10,
     backgroundColor: theme.colors.appBarBackground,
-    height: 80,
-    gap: 15
+    height: 80
   },
   text: {
     color: theme.colors.appBarText
+  },
+  scrollView: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: 10,
+    gap: 15
   }
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link to='/'>
-        <Subheading style={styles.text}>Repositories</Subheading>
-      </Link>
+      <ScrollView horizontal contentContainerStyle={styles.scrollView}>
+        <Link to='/'>
+          <Subheading style={styles.text}>Repositories</Subheading>
+        </Link>
 
-      <Link to='/sign-in'>
-        <Subheading style={styles.text}>Sign in</Subheading>
-      </Link>
+        <Link to='/sign-in'>
+          <Subheading style={styles.text}>Sign in</Subheading>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
